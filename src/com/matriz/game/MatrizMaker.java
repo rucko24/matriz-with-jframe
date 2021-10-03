f68cf076058b4094caff19d8b7506fb74b317cd0
rucko24 <rucko24@gmail.com>
2021-10-03 22:16:18 +0200

    :memo: :bug:

--- src/com/matriz/game/MatrizMaker.java
+++ src/com/matriz/game/MatrizMaker.java
@@ -15,13 +15,13 @@ import java.awt.GridLayout;
  */
 public class MatrizMaker extends GridLayout {
     private final JFrame jFrame;
-    private int row;
-    private int cols;
+    private int filas;
+    private int columnas;
 
-    public MatrizMaker(final JFrame jFrame, final int row, final int cols) {
-        super(row, cols);
-        this.row = row;
-        this.cols = cols;
+    public MatrizMaker(final JFrame jFrame, final int filas, final int columnas) {
+        super(filas, columnas);
+        this.filas = filas;
+        this.columnas = columnas;
         this.jFrame = jFrame;
 
         this.matriz();
@@ -33,11 +33,11 @@ public class MatrizMaker extends GridLayout {
 
     private void fillMatriz() {
         int count = 1;
-        for(int filas=0; filas<row; filas++) {
-            for(int columnas=0; columnas<cols; columnas++) {
+        for(int f=0; f<filas; f++) {
+            for(int c=0; c<columnas; c++) {
                 JPanel panel = centerJLabel(new JLabel(String.valueOf(count++)));
-                if(filas % 2 == 0) {
-                    //Add background color
+                if(f % 2 == 0) {
+                    //Add example background color
                     panel.setBackground(Color.blue);
                 }
                 this.jFrame.add(panel);
